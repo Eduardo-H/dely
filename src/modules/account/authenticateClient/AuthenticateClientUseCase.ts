229c94555f454f75f70c1127bf676ab88b3ee105
Eduardo <edu-henrique2014@hotmail.com>
2022-03-31 22:47:45 -0300

    Authenticate client use case implemented

---
+++ src/modules/account/authenticateClient/AuthenticateClientUseCase.ts
@@ -0,0 +1,33 @@
+import { prisma } from '../../../database/prismaClient';
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+
+interface IAuthenticateClient {
+  username: string;
+  password: string;
+}
+
+export class AuthenticateClientUseCase {
+  async execute({ username, password }: IAuthenticateClient) {
+    const client = await prisma.client.findFirst({
+      where: { 
+        username
+      }
+    });
+
+    if (!client)
+      throw new Error('Username or password incorrect.');
+
+    const passwordMatches = await compare(password, client.password);
+
+    if (!passwordMatches)
+      throw new Error('Username or password incorrect.');
+
+    const token = sign({ username }, 'a60e64df52acdef32af9cc2b7b47748c', {
+      subject: client.id,
+      expiresIn: '1d'
+    });
+
+    return token;
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
 
 import { CreateClientController } from './modules/client/useCases/createClient/CreateClientController';
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
+const authenticateClientController = new AuthenticateClientController();
+
+routes.post('/client/authenticate', authenticateClientController.handle);
 
 routes.post('/client', createClientController.handle);
 
