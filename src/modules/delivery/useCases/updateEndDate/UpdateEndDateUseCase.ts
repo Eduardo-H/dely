9d9987e65ad4f5902bb8674b7eefa427d6ad2dca
Eduardo <edu-henrique2014@hotmail.com>
2022-04-04 13:51:31 -0300

    Update delivery end date use case updated

--- src/modules/delivery/useCases/updateEndDate/UpdateEndDateUseCase.ts
+++ src/modules/delivery/useCases/updateEndDate/UpdateEndDateUseCase.ts
@@ -7,16 +7,24 @@ interface IUpdateEndDate {
 
 export class UpdateEndDateUseCase {
   async execute({ id_delivery, id_deliveryman }: IUpdateEndDate) {
-    const delivery = await prisma.delivery.updateMany({
+    const delivery = await prisma.delivery.findFirst({
       where: {
-        id: id_delivery,
-        id_deliveryman
+        id: id_delivery
+      }
+    });
+
+    if (delivery?.id_deliveryman !== id_deliveryman)
+     throw new Error('Deliveryman is different from the original delivery.');
+
+    const updatedDelivery = await prisma.delivery.update({
+      where: {
+        id: id_delivery
       },
       data: {
         ended_at: new Date()
       }
     });
 
-    return delivery;
+    return updatedDelivery;
   }
 }
