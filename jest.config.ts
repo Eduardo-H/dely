a6ecb7695550934ed16a23518ee610ee6fb05e05
Eduardo-H <edu-henrique2014@hotmail.com>
2022-10-19 11:44:03 -0300

    E2E testing configured

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,10 @@
-module.exports = {
+import type { Config } from '@jest/types';
+
+export default {
   bail: true,
   clearMocks: true,
   maxWorkers: 1,
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/singleton.ts'],
-}
+  testRegex: '.*\\.spec\\.ts$',
+} as Config.InitialOptions;
